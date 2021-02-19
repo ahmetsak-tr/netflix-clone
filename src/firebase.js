ca1afb1d94aa6431a5aa176c44e816274f00c28e
ahmetsak-tr <ahmeterensak@outlook.com>
2021-02-19 18:27:29 +0300

    lastupdate
    lastupdate

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5eSX4sl8S9ql6z8RK2ggpwRkVRPlG93I",
+    authDomain: "netflix-clone-9436e.firebaseapp.com",
+    projectId: "netflix-clone-9436e",
+    storageBucket: "netflix-clone-9436e.appspot.com",
+    messagingSenderId: "907971020381",
+    appId: "1:907971020381:web:d74bf7851497531acb6fb4"
+  };
